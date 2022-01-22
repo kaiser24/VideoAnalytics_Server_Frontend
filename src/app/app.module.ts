f993474e4741cc8ad1eef418946b571edc45b943
kaiser24 <damsog38@gmail.com>
2022-01-22 17:51:40 -0500

    Started main_menu component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { NavBar } from './home/navbar/navbar.component';
+import { NavBarComponent } from './home/navbar/navbar.component';
+import { MainMenuComponent } from './home/main_menu/main_menu.component';
 import { HomeComponent } from './home/home.component';
 import { LoginViewComponent } from './loginview/loginview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBar,
+    NavBarComponent,
+    MainMenuComponent,
     HomeComponent,
     LoginViewComponent
   ],
---
+++ src/app/home/main_menu/main_menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-main_menu',
+    templateUrl: './main_menu.component.html',
+    styleUrls: ['./main_menu.component.css']
+})
+export class MainMenuComponent {
+
+}
