c97fbb32d840b51b15c871e6c7a9838caf547348
kaiser24 <damsog38@gmail.com>
2022-01-22 16:43:36 -0500

    Created a new component for the top navbar element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavBar } from './home/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginViewComponent } from './loginview/loginview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavBar,
     HomeComponent,
     LoginViewComponent
   ],
