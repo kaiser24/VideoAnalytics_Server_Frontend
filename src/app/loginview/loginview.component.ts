8d48c4133e4ddec1ac172d981395baadcc64b667
kaiser24 <damsog38@gmail.com>
2022-01-21 23:14:07 -0500

    Starting the login view

---
+++ src/app/loginview/loginview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-loginview',
+    templateUrl: './loginview.component.html'
+})
+export class LoginViewComponent {
+    sometext = "hey"
+}
