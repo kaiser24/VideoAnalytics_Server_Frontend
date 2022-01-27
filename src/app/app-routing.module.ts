f379ab7c69222f1d3ed3115a8b175982ecaac1ff
kaiser24 <damsog38@gmail.com>
2022-01-26 21:11:17 -0500

    Created facedetection component and working on it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginViewComponent } from './loginview/loginview.component';
+import { FaceDetectionComponent } from './facedetection/facedetection.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'facedetection', component: FaceDetectionComponent},
   {path: 'login', component: LoginViewComponent},
 ];
 
---
+++ src/app/facedetection/facedetection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-facedetection',
+    templateUrl: './facedetection.component.html',
+    styleUrls: ['./facedetection.component.css']
+})
+export class FaceDetectionComponent {
+
+}
