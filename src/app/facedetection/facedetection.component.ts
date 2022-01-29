a7d699937c639afe9e7ff8acf017c6631c34faae
kaiser24 <damsog38@gmail.com>
2022-01-29 16:16:31 -0500

    Working on rtcp connection. Added functions to update sdp for codec and video settings. also working on negotiation

--- src/app/facedetection/facedetection.component.ts
+++ src/app/facedetection/facedetection.component.ts
@@ -8,14 +8,17 @@ import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 export class FaceDetectionComponent {
     // Members Declarations
     pc!: RTCPeerConnection;
-    iceGatheringLog: String = 'iceGathering: ';
-    iceConnectionLog: String = 'iceConnection: ';
-    signalingLog: String = 'signaling: ';
+    iceGatheringLog: string = 'iceGathering: ';
+    iceConnectionLog: string = 'iceConnection: ';
+    signalingLog: string = 'signaling: ';
     // DOM elements
     @ViewChild('video') video!: ElementRef;
-    @ViewChild('video-resolution') resolution_element!: ElementRef;
+    //@ViewChild('video-resolution') resolution_element!: ElementRef;
+    //@ViewChild('video-codec') video_codec!: ElementRef;
 
     // Class Methods
+
+    // Creating the peer connection and returns the peer connection object configured
     createPeerConnection():RTCPeerConnection {
         let config: any;
         config= { sdpSemantics: 'unified-plan' };
@@ -46,17 +49,57 @@ export class FaceDetectionComponent {
         return pc;
     }
 
+    // Negotiating between peers through the Signaling Server
+    negotiate(){
+        async () => {
+            // Need to create the RTC offer, then . 
+            try{
+                let offer: RTCLocalSessionDescriptionInit = await this.pc.createOffer();
+                this.pc.setLocalDescription(offer);
+                await new Promise<void>( (resolve) => {
+                    if (this.pc.iceGatheringState === 'complete'){
+                        resolve();
+                    } else {
+                        // Will this work?
+                        this.pc.onicegatheringstatechange = ev => {
+                            if (this.pc.iceGatheringState === 'complete'){
+                                resolve();
+                            }
+                        };
+                    }
+                });
+
+                let codec:string = 'default';
+                
+                if(codec !== 'default' ){
+                    offer.sdp = this.sdpFilterCodec('video', codec, offer.sdp);
+                }
+
+                // TODO: SDP offer petition.
+
+
+            }catch (e) {
+
+            }
+            
+        }
+
+        
+    }
+    
+    // Function to start Wertc connection process
     start() {
-        let resolution: String;
-        let resolution_vals: String[];
-        //let constraints: { [id: string]: boolean, [id: number]: boolean } = {};
+        let resolution: string;
+        let resolution_vals: string[];
         let constraints: any;
         this.pc = this.createPeerConnection();
 
+        // Setting up parameters
         constraints = { audio: false, video: false };
-        resolution = this.resolution_element.nativeElement.value;
+        //resolution = this.resolution_element.nativeElement.value;
+        resolution = "960x540";
 
-        if (resolution) {
+        if (resolution) { 
             resolution_vals = resolution.split('x');
             constraints['video'] = {
                 width: parseInt(resolution[0], 0),
@@ -66,22 +109,81 @@ export class FaceDetectionComponent {
             constraints.video = true;
         }
 
+        // Getting user Media (Critic PArt. Still a bit confusing)
         if (constraints.audio || constraints.video) {
-            if (constraints.video) {
-                //document.getElementById('media').style.display = 'block';
+
+            async function getMedia(pc: RTCPeerConnection){ 
+                try { 
+                    let stream: MediaStream = await navigator.mediaDevices.getUserMedia(constraints) ;
+                    stream.getTracks().forEach( (track) => { pc.addTrack(track, stream) } );
+                } catch (error) {
+                    console.log(error);
+                }
             }
-            navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-                stream.getTracks().forEach(function(track) {
-                    //this.pc.addTrack(track, stream);
-                });
-                //return negotiate();
-            }, function(err) {
-                alert('Could not acquire media: ' + err);
-            });
         } else {
-            //negotiate();
+            this.negotiate();
         }
             
 
+    }
+
+    sdpFilterCodec(kind:string, codec:string, realSdp:any) {
+        var allowed:number[] = [];
+        var rtxRegex:RegExp = new RegExp('a=fmtp:(\\d+) apt=(\\d+)\r$');
+        var codecRegex:RegExp = new RegExp('a=rtpmap:([0-9]+) ' + this.escapeRegExp(codec))
+        var videoRegex:RegExp = new RegExp('(m=' + kind + ' .*?)( ([0-9]+))*\\s*$')
+        var lines:string[] = realSdp.split('\n');
+        var isKind:boolean = false;
+
+        for (var i = 0; i < lines.length; i++) {
+            if (lines[i].startsWith('m=' + kind + ' ') ){
+                isKind = true;
+            } else if (lines[i].startsWith('m=')){
+                isKind = false;
+            }
+
+            if (isKind) {
+                var match:RegExpMatchArray|null = lines[i].match(codecRegex);
+                if (match) {
+                    allowed.push(parseInt(match[1]));
+                }
+
+                match = lines[i].match(rtxRegex);
+                if (match && allowed.includes(parseInt(match[2])) ){
+                    allowed.push(parseInt(match[1]));
+                }
+            }
+        }
+
+        var skipRegex:RegExp = new RegExp('a=(fmtp|rtcp-fb|rtpmap):([0-9]+)');
+        var sdp:string = '';
+
+        isKind = false;
+        for (var i = 0; i < allowed.length; i++) {
+            if (lines[i].startsWith('m=' + kind + ' ')) {
+                isKind = true;
+            } else if (lines[i].startsWith('m=')) {
+                isKind = false;
+            }
+    
+            if (isKind) {
+                var skipMatch:RegExpMatchArray|null = lines[i].match(skipRegex);
+                if (skipMatch && !allowed.includes(parseInt(skipMatch[2]))) {
+                    continue;
+                } else if (lines[i].match(videoRegex)) {
+                    sdp += lines[i].replace(videoRegex, '$1 ' + allowed.join(' ')) + '\n';
+                } else {
+                    sdp += lines[i] + '\n';
+                }
+            } else {
+                sdp += lines[i] + '\n';
+            }
+        }
+    
+        return sdp;
+    }
+
+    escapeRegExp(string:string) {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
     }
 }
