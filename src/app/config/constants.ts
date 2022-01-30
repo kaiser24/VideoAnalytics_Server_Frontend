d253ab4176e1d8040172fcc17dbeb29218bb31bd
kaiser24 <damsog38@gmail.com>
2022-01-30 13:29:31 -0500

    Fixed api endpoint

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Constants {
-    public readonly API_ENDPOINT: string = 'https://192.168.1.5:4000/';
+    public readonly API_ENDPOINT: string = 'http://192.168.1.5:4000/api';
     public static TitleOfSite: string = "Gnosis";
 }
