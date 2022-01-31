94ce1f57d1572597d638a36dc9c9710c7546fbce
kaiser24 <damsog38@gmail.com>
2022-01-30 21:00:50 -0500

    Fixed AuthGuard to set the token on the jwt service before checking it

--- src/app/services/authguard.service.ts
+++ src/app/services/authguard.service.ts
@@ -2,28 +2,33 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { JWTTokenService } from './jwt.service';
 import { Router } from '@angular/router';
+import { LocalStorageService } from './localstorage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizeGuard implements CanActivate {
-  constructor(  
-    private router: Router, 
-    private jwtService: JWTTokenService) {
-  }
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-      if (this.jwtService.getUser()) {
-        if (this.jwtService.isTokenExpired()) {
-            this.router.navigate(['/login']);
-            return false;
-        } else {
-            return true;
+    constructor(  
+        private router: Router, 
+        private jwtService: JWTTokenService,
+        private localstorage: LocalStorageService
+    ) {}
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): boolean {
+            this.jwtService.setToken(this.localstorage.get("token") as string);
+            console.log(this.localstorage.get("token"));
+            console.log(this.jwtService.getUser());
+            if (this.jwtService.getUser()) {
+                if (this.jwtService.isTokenExpired()) {
+                    this.router.navigate(['/login']);
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                this.router.navigate(['/login']);
+                return false;
+            }
         }
-      } else {
-        this.router.navigate(['/login']);
-        return false;
-      }
-  }
 }
