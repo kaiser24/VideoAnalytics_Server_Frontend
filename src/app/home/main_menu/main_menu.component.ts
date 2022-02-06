aa37242839c19fb9d41251de66eb38319d9c1507
kaiser24 <damsog38@gmail.com>
2022-02-06 13:01:46 -0500

    Added redirect button to navigate towards facedetection view

--- src/app/home/main_menu/main_menu.component.ts
+++ src/app/home/main_menu/main_menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-main_menu',
@@ -6,5 +7,11 @@ import { Component } from "@angular/core";
     styleUrls: ['./main_menu.component.css']
 })
 export class MainMenuComponent {
+    constructor(private router: Router) {}
+
+    navigatetoFacedetection(): void {
+        console.log("Selected Facedetection");
+        this.router.navigate(["/facedetection"]);
+    }
 
 }
