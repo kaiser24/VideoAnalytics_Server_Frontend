ef36e194c1d1aeb8a339f1d0f916d53f597c3f66
kaiser24 <damsog38@gmail.com>
2022-02-07 23:04:32 -0500

    Added facedetectionlive component

---
+++ src/app/facedetection/facedetectionlive/facedetectionlive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facedetectionlive',
+  templateUrl: './facedetectionlive.component.html',
+  styleUrls: ['./facedetectionlive.component.css']
+})
+export class FacedetectionliveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
