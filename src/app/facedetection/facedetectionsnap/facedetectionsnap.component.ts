c77e720d3d173fec760541665fe9cbb80fec00e2
kaiser24 <damsog38@gmail.com>
2022-02-07 23:04:56 -0500

    added facedetectionsnap component

---
+++ src/app/facedetection/facedetectionsnap/facedetectionsnap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facedetectionsnap',
+  templateUrl: './facedetectionsnap.component.html',
+  styleUrls: ['./facedetectionsnap.component.css']
+})
+export class FacedetectionsnapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
